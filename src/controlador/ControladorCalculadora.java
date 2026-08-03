5b03a0966456e385002527851fb07575777faa00
Maria <mafloresur@est.utn.ac.cr>
2026-08-03 15:43:46 -0600

    Se agrego la funcion para agregar numero1 y numero2

--- src/controlador/ControladorCalculadora.java
+++ src/controlador/ControladorCalculadora.java
@@ -13,7 +13,6 @@ import javax.swing.JOptionPane;
 public class ControladorCalculadora {
     private FrmCalculadora vista;
     private Operaciones modelo;
-
     private double numero1;
     private double numero2;
     private String operacion;
@@ -24,55 +23,64 @@ public class ControladorCalculadora {
         numero1 = 0;
         numero2 = 0;
         operacion = "";
+
         agregarEventos();
     }
 
 
     private void agregarEventos() {
-
     }
 
 
     public void seleccionarOperacion(String op) {
-        if (numero1 == 0) {
+        if(numero1 == 0){
             JOptionPane.showMessageDialog(null,"Debe ingresar el primer número.");
             return;
         }
         operacion = op;
     }
-
     public double calcular() {
         double resultado = 0;
         try {
             modelo.setResultado(numero1);
             modelo.setValor(numero2);
-            switch (operacion) {
+            
+            switch(operacion){
                 case "+":
                     modelo.sumar();
                     break;
+
                 case "-":
                     modelo.restar();
                     break;
+
                 case "*":
                     modelo.multiplicar();
                     break;
+
                 case "/":
                     modelo.dividir();
                     break;
 
                 default:
-                    JOptionPane.showMessageDialog(null,"Seleccione una operación.");
-            }
-            resultado = modelo.getResultado();
-        } catch (ArithmeticException e) {
 
+                    JOptionPane.showMessageDialog(null,
+                    "Seleccione una operación.");
+
+                    break;
+            }
+
+
+            resultado = modelo.getResultado();
+        } catch(ArithmeticException e){
             JOptionPane.showMessageDialog(null,e.getMessage());
+
         }
         return resultado;
     }
 
 
-    public void limpiarTodo() {
+    public void limpiarTodo(){
         numero1 = 0;
         numero2 = 0;
         operacion = "";
@@ -80,10 +88,10 @@ public class ControladorCalculadora {
 
     }
 
+    public void setNumero1(double numero1){
+        this.numero1 = numero1;
+    }
+    public void setNumero2(double numero2){
+        this.numero2 = numero2;
+    }
 }
-
-
-
-
-    
-    
